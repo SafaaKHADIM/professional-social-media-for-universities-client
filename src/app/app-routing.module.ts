09959b2091ffee99802c88600b2aa2ae966108d1
Younes Najjar <younes.najjar.96@gmail.com>
2019-12-08 07:28:17 +0200

    Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/pending-requests/home',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'home',
+  //   redirectTo: '/pending-requests/home',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'authentication',
+  //   loadChildren: './authentication/authentication.module#AuthenticationModule',
+  // },
+  {path: 'home', loadChildren: './home/home.module#HomeModule'},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
