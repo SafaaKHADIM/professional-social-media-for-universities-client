a9f0924edee1371d21d673a470795582c5d7ca06
Younes Najjar <younes.najjar.96@gmail.com>
2019-12-08 20:07:53 +0200

    Navigations

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  navigateToProfile() {
+      this.router.navigateByUrl('/profile');
+  }
+  navigateToChat() {
+    this.router.navigateByUrl('/chat');
+  }
+  navigateToBlog() {
+    this.router.navigateByUrl('/blog');
+  }
 }
